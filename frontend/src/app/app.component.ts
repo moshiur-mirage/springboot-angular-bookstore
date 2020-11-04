1a5e579165afbd6f984a332f89c820587b59e80a
Opu Rahman <moshiur.mirage@gmail.com>
2020-11-05 02:09:34 +0600

    started angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { Book } from './common/book';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'frontend';
+  books: Book[] = [
+    {
+      sku: 'text-101',
+      name: 'C# Crash course',
+      description: 'Learn C# Programming Language',
+      unitPrice: 800,
+      imageUrl: 'assets/image/books/text-101,png',
+      active: true,
+      unitsInStock: 100,
+      createdOn: new Date(),
+      updatedOn: null,
+    },
+    {
+      sku: 'text-102',
+      name: 'Python Crash course',
+      description: 'Learn C# Programming Language',
+      unitPrice: 800,
+      imageUrl: 'assets/image/books/text-101,png',
+      active: true,
+      unitsInStock: 100,
+      createdOn: new Date(),
+      updatedOn: null,
+    },
+    {
+      sku: 'text-103',
+      name: 'Java Crash course',
+      description: 'Learn C# Programming Language',
+      unitPrice: 800,
+      imageUrl: 'assets/image/books/text-101,png',
+      active: true,
+      unitsInStock: 100,
+      createdOn: new Date(),
+      updatedOn: null,
+    },
+  ];
 }
