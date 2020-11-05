845945a0b5c1110f687ef7c88c71c0c45d42a0b4
Opu Rahman <moshiur.mirage@gmail.com>
2020-11-05 20:24:56 +0600

    get call from service

--- bookstore/src/main/java/com/moshiur/bookstore/category/BookCategoryRepository.java
+++ bookstore/src/main/java/com/moshiur/bookstore/category/BookCategoryRepository.java
@@ -2,7 +2,9 @@ package com.moshiur.bookstore.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "bookCategory",path = "book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory,Long> {
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookService } from './services/book.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
